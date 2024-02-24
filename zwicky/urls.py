7386bd2f10e1639d2203c6312b4aa88a0a704c14
James Reed <james@twiddlingbits.net>
2024-02-24 15:13:25 -0700

    Add logout and login required decorator

--- zwicky/urls.py
+++ zwicky/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.signup, name="signup"),
-    path("account/", include("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
--- zwicky/views.py
+++ zwicky/views.py
@@ -1,9 +1,10 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 from . import forms
 
 
+@login_required
 def index(request):
     return render(request, "index.html")
 
